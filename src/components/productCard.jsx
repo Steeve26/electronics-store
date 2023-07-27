1b652753377fa2dc9a4ea9c74308b602b73a2bc0
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-07-28 02:19:23 +0300

    minor styling + added product card buttons

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {MdOutlineShoppingCart} from 'react-icons/md'
+import {AiFillHeart, AiOutlineHeart} from 'react-icons/ai'
 
 export default function productCard() {
+  const [liked, setLiked] = useState(false);
+
   return (
-    <div className="product-card"></div>
+    <div className="product-card">
+      <div className="buttons">
+        <button><MdOutlineShoppingCart size={20}/></button>
+        <button onClick={ () => setLiked(!liked)}>
+          { liked ? <AiFillHeart size={20} color={'red'}/> : <AiOutlineHeart size={20}/>}
+        </button>
+      </div>
+    </div>
   )
 }
